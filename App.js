4d780bdd6c1c4c97f4641f92546aa244ac99e8df
artomtankist2000 <148547297+artomtankist2000@users.noreply.github.com>
2024-01-04 19:56:19 +0500

    Create App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import GameBoard from './components/GameBoard';
+
+function App() {
+  return (
+
+    <GameBoard/>
+  );
+}
+
+export default App;
